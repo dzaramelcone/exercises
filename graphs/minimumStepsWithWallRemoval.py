5d82589739aca832d5467aebcb88952b6b5189e4
Dzara Melcone <dzaramelcone@gmail.com>
2024-11-30 13:54:29 -0500

    simplifies remove k walls, in-lines functions

--- graphs/minimumStepsWithWallRemoval.py
+++ graphs/minimumStepsWithWallRemoval.py
@@ -26,63 +26,39 @@ module.exports = {
   minimumStepsWithWallRemoval,
 }
 '''
-
-class Node:
-  def __init__(self, pos, matrix, parent=None):
-    self.pos = pos
-    x, y = pos
-    self.steps = parent.steps + 1 if parent else 0
-    self.k = parent.k - matrix[y][x] if parent else 0
-    self.distance = (len(matrix[0])-1-x) + (len(matrix)-1-y)
-
-  def __lt__(self, other):
-    if self.distance < other.distance:
-      return True
-    if self.distance + self.steps < other.distance + other.steps:
-      return True
-    if self.distance + self.steps + self.k < other.distance + other.steps + other.k:
-      return True
-    return self.pos > other.pos
-
-import heapq
+from collections import deque
 
 def minimum_steps_with_wall_removal(matrix, removal):
   n, m = len(matrix[0]), len(matrix)
-  def in_bounds(pos):
-    x, y = pos
-    return 0 <= x < n and 0 <= y < m
-  def get_adj(pos):
-    x, y = pos
-    return [(x+1,y), (x,y+1), (x-1,y), (x,y-1)]
-  def get_neighbors(node):
-    return [Node((u,v), matrix, node) for u, v in get_adj(node.pos) if in_bounds((u, v))]
-  def get_distance(node):
-    x, y = node.pos
-    return 
-
-  pq = []
-  home = Node((0,0), matrix)
-  home.k = removal
-  heapq.heappush(pq, home)
+  ex, ey = n-1, m-1
+  dirs = [(1,0), (0,1), (-1,0), (0,-1)]
+  queue = deque()
   visited = set()
-  while pq:
-    cur = heapq.heappop(pq)
-    if cur.pos == (n-1, m-1):
-      return cur.steps
-    for neighbor in get_neighbors(cur):
-      if neighbor.k < 0:
-        continue
-      # worst case is now n*m*k memory to support this..
-      # is there some better solution?
-      # maybe some kinda percolation solution..
 
-      # uhh, thought: could you just do bfs with this instead?
-      grp = (neighbor.pos, neighbor.k)
-      if grp in visited:
-        continue
-      visited.add(grp)
-      heapq.heappush(pq, neighbor)
+  if removal >= m + n - 3:
+      return m + n - 2
+
+  queue.append((0,0,removal))
+  dist = 0
+  while queue:
+    sz = len(queue)
+    for _ in range(sz):
+      u, v, w = queue.popleft()
+      if u == ex and v == ey:
+        return dist
+      for x, y in dirs:
+        p, q = u+x, v+y
+        if not (0 <= p < n and 0 <= q < m):
+          continue
+        if (r := w - matrix[q][p]) < 0:
+          continue
+        if (loc := (p, q, r)) in visited:
+          continue
+        visited.add(loc)
+        queue.append(loc)
+    dist += 1
   return -1
+        
 
 map = [
     [0, 1, 0, 0, 0],
