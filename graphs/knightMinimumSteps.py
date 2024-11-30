64888516689f6ed46152449024dd1da96751b74f
Dzara Melcone <dzaramelcone@gmail.com>
2024-11-29 21:36:24 -0500

    creates tests, fixes a bug due to duplicate variable name

--- graphs/knightMinimumSteps.py
+++ graphs/knightMinimumSteps.py
@@ -14,6 +14,8 @@ module.exports = {
 '''
 from collections import deque
 def knight_min_steps(size, startPos, targetPos):
+  # this can prob be done in constant time/memory with a lookup.
+  # or even a way to just compute the value using almost no memory with an equation, since each octant is symmetric.
   def in_bounds(pos):
     x, y = pos
     return 0 <= x < size and 0 <= y < size
@@ -33,8 +35,8 @@ def knight_min_steps(size, startPos, targetPos):
 
   dist = 0
   while queue:
-    size = len(queue)
-    for _ in range(size):
+    sz = len(queue)
+    for _ in range(sz):
       cur = queue.popleft()
         # a slight optimization could be to return this in the get_moves loop instead.
         # but if the start is the target then we need to check for that.
@@ -43,10 +45,15 @@ def knight_min_steps(size, startPos, targetPos):
       for loc in get_moves(cur):
         if loc in visited or not in_bounds(loc):
           continue
+        
         queue.append(loc)
         visited.add(loc)
     dist += 1
   return -1
 
 
-
+print(knight_min_steps(8, (3,3), (4,5)))
+print(knight_min_steps(8, (3,3), (4,4)))
+print(knight_min_steps(8, (3,3), (3,4)))
+print(knight_min_steps(8, (3,3), (5,5)))
+print(knight_min_steps(8, (0,0), (7,7)))
