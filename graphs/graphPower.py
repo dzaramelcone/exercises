d139868cc3eae537a4af21b93dd07eb7e98ffa2d
Dzara Melcone <dzaramelcone@gmail.com>
2024-11-29 22:11:12 -0500

    adds tests, simplifies the bfs a lot but the runtime is probably worse

--- graphs/graphPower.py
+++ graphs/graphPower.py
@@ -21,30 +21,38 @@ from collections import deque
 def graph_power(graph, k):
   queue = deque()
   visited = set()
-  updated = set()
   ans = {v:[] for v in graph}
   for v in graph:
-    if v in updated:
-      continue
     dist = 0
     visited.clear()
     queue.append(v)
     while queue:
       size = len(queue)
-      if dist > k:
+      if dist >= k:
         break
       for _ in range(size):
         cur = queue.popleft()
-        visited.add(edge)
         for edge in graph[cur]:
           if edge not in visited:
             queue.append(edge)
+            visited.add(edge)
       dist += 1
-    
     queue.clear()
-    updated.extend(visited)
-    for v in visited:
-      ans[v].extend(visited)
+    ans[v].extend(visited)
 
   return ans
 
+g = {
+   1: [2, 3, 4],
+   2: [1, 5],
+   3: [1, 4],
+   4: [1, 3, 5],
+   5: [2, 4, 6],
+   6: [5, 7],
+   7: [6]
+
+}
+print(graph_power(g, 0))
+print(graph_power(g, 1))
+print(graph_power(g, 2))
+print(graph_power(g, 3))
